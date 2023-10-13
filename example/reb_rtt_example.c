c2a9e8e2d5ff925265cc1e4ebb5e7011e91154d1
RiceChen <chenjiafan24367@talkweb.com.cn>
2023-10-13 15:20:27 +0800

    增加测试用例

--- example/reb_rtt_example.c
+++ example/reb_rtt_example.c
@@ -1,27 +1,60 @@
-#include "revent_bus.h"
+#include "rtthread.h"
+#include "reb_broker.h"
+#include "reb_observer.h"
+#include "reb_publisher.h"
 
-void reb_recv_handler(const char *event, void *payload, uint16_t lenght)
+observer_base *obs_signal;
+observer_base *obs_call;
+observer_base *obs_task;
+
+void sig_thread_handle(void *arg)
 {
-    rt_kprintf("Recv: event: %s, payload: %.*s\r\n", event, lenght, payload);
+    while(1) {
+        if(observer_signal_wait(obs_signal, RT_WAITING_FOREVER) == REB_OK) {
+            rt_kprintf("signal: recv success\r\n");        
+        }
+    }
 }
+
+void obs_callback(uint32_t event, uint32_t data, void *arg)
+{
+    rt_kprintf("call: event: 0x%08x, data: %s\r\n", event, (char *)data);
+}
+
+void obs_task_fun(uint32_t event, uint32_t data, void *arg)
+{
+    rt_kprintf("task: event: 0x%08x, data: %s\r\n", event, (char *)data);
+}
+
+int reb_init(void)
+{
+    rt_thread_t signal_thread = NULL;
+
+    broker_create();
+
+    obs_signal = observer_signal_create(1, REB_ALL_MINOR_TYPE);
+
+    signal_thread = rt_thread_create("sig_thread", sig_thread_handle, NULL, 1024, 10, 20);
+    rt_thread_startup(signal_thread);
+
+    obs_call = observer_callback_create(1, REB_ALL_MINOR_TYPE, obs_callback, NULL);
+
+    obs_task = observer_task_create(1, REB_ALL_MINOR_TYPE, obs_task_fun, NULL, 1024, 15);
+
+    broker_observer_attach(obs_signal);
+    broker_observer_attach(obs_call);
+    broker_observer_attach_once(obs_task);
+
+    return RT_EOK;
+}
+INIT_COMPONENT_EXPORT(reb_init);
 
 int reb_test(void)
 {
-    reb_init();
-
-    reb_subscribe("event1", reb_recv_handler);
-    reb_subscribe("event2", reb_recv_handler);
-
-    reb_info_dump();
-
-    while (1)
-    {
-        reb_publish("event1", "Rice: event bus", rt_strlen("Rice: event bus"));
-        reb_publish("event2", "Publish sync msg", rt_strlen("Publish sync msg"));
-        rt_thread_delay(100);
-        reb_async_publish("event1", REB_PRIORITY_ORDINARY, "Good Good", rt_strlen("Good Good"));
-        reb_async_publish("event2", REB_PRIORITY_ORDINARY, "Publish async msg", rt_strlen("Publish async msg"));
-        rt_thread_delay(100);
-    }
+    char *data = "RiceChen";
+    publisher_send(1, 1, (int)data, 1000);
+    publisher_send(1, 2, (int)data, 1000);
+    publisher_send(1, 3, (int)data, 1000);
 }
-INIT_COMPONENT_EXPORT(reb_test);
+MSH_CMD_EXPORT(reb_test, Rice Event broker test);
+
